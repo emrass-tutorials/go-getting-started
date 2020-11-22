d075d40443d46b970605d143b6e0e6ca22ad44a2
Enrico Mrass <emrass@users.noreply.github.com>
2020-11-22 07:05:33 +0000

    Using methods to add behavior to a type

--- models/user.go
+++ models/user.go
@@ -10,3 +10,14 @@ var (
 	users  []*User
 	nextId = 1
 )
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(u User) (User, error) {
+	u.ID = nextId
+	nextId++
+	users = append(users, &u)
+	return u, nil
+}
